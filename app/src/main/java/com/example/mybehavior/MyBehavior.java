2279ef69bf448abee23bfc1b6e7263be379df392
apple <18201497620@163.com>
2020-10-06 00:49:40 +0800

    布局页面加入

---
+++ app/src/main/java/com/example/mybehavior/MyBehavior.java
@@ -0,0 +1,30 @@
+package com.example.mybehavior;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+/**
+ * create by libo
+ * create on 2020/10/5
+ * description
+ */
+public class MyBehavior extends CoordinatorLayout.Behavior<View> {
+
+    public MyBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(@NonNull CoordinatorLayout parent, @NonNull View child, @NonNull View dependency) {
+        return super.layoutDependsOn(parent, child, dependency);
+    }
+
+    @Override
+    public boolean onDependentViewChanged(@NonNull CoordinatorLayout parent, @NonNull View child, @NonNull View dependency) {
+        return super.onDependentViewChanged(parent, child, dependency);
+    }
+}
